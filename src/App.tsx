cd8dfdb73e31db3882577a88898ebead4e2c68de
Olena Kolesnik <lelya6350@gmail.com>
2026-03-12 23:08:43 +0200

    made formik

--- src/App.tsx
+++ src/App.tsx
@@ -33,59 +33,69 @@
 // export default App;
 
 // Пагінація 
-import { useState } from "react";
-import SearchForm from "./components/SearchForm/SeachForm";
-import ArticleList from "./components/ArticleList/ArticleList";
-import { keepPreviousData,  useQuery } from "@tanstack/react-query";
-import { fetchArticles } from "./services/article";
-import ReactPaginate from "react-paginate";
-import css from "./App.module.css";
+// import { useState } from "react";
+// import SearchForm from "./components/SearchForm/SeachForm";
+// import ArticleList from "./components/ArticleList/ArticleList";
+// import { keepPreviousData,  useQuery } from "@tanstack/react-query";
+// import { fetchArticles } from "./services/article";
+// import ReactPaginate from "react-paginate";
+// import css from "./App.module.css";
 
 
-function App() {
-  const [search, setSearch] = useState<string | null>(null);
-  const [page, setPage] = useState(0);
-  const { data, error, isLoading, isError, isSuccess } = useQuery({
-    queryKey: ["articles", search, page],
-    queryFn: () => fetchArticles(search as string, page),
-    enabled: search !== null,
-    placeholderData: keepPreviousData,
-  });
-  const handleSearch = (topic: string) => {
-    setSearch(topic);
-    setPage(0);
-  }
+// function App() {
+//   const [search, setSearch] = useState<string | null>(null);
+//   const [page, setPage] = useState(0);
+//   const { data, error, isLoading, isError, isSuccess } = useQuery({
+//     queryKey: ["articles", search, page],
+//     queryFn: () => fetchArticles(search as string, page),
+//     enabled: search !== null,
+//     placeholderData: keepPreviousData,
+//   });
+//   const handleSearch = (topic: string) => {
+//     setSearch(topic);
+//     setPage(0);
+//   }
 
-  const handlePageChange = ({selected}: {selected: number}) => {
-setPage(selected);
+//   const handlePageChange = ({selected}: {selected: number}) => {
+// setPage(selected);
 
-  }
+//   }
+//   return (
+//     <>
+//       <SearchForm onSearch={handleSearch} />
+//       {isLoading && <p>Loading...</p>}
+//       {isError && (
+//         <>
+//           <h3>Oops! Something went wrong</h3>
+//           <p>{error.message}</p>
+//         </>
+//       )}
+//       {isSuccess && <>
+//         <ArticleList items={data.hits} />
+//         <ReactPaginate
+//           nextLabel=">"
+//           previousLabel="<"
+//           forcePage={page}
+//           pageCount={data.nbPages}
+//           containerClassName={css.pagination}
+//           activeClassName={css.active}
+//           onPageChange={handlePageChange}
+//         />
+//         </>
+//         }
+
+//     </>
+//   )
+// }
+
+// export default App;
+
+import OrderForm from "./components/OrderForm/OrderForm";
+
+export default function App() {
   return (
     <>
-      <SearchForm onSearch={handleSearch} />
-      {isLoading && <p>Loading...</p>}
-      {isError && (
-        <>
-          <h3>Oops! Something went wrong</h3>
-          <p>{error.message}</p>
-        </>
-      )}
-      {isSuccess && <>
-        <ArticleList items={data.hits} />
-        <ReactPaginate
-          nextLabel=">"
-          previousLabel="<"
-          forcePage={page}
-          pageCount={data.nbPages}
-          containerClassName={css.pagination}
-          activeClassName={css.active}
-          onPageChange={handlePageChange}
-        />
-        </>
-        }
-
+    <OrderForm />
     </>
   )
 }
-
-export default App;
